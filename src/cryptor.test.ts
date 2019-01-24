bae57c3ff4cff306b839d414689b4ec012210df0
Wichert Akkerman <wichert@wiggy.net>
2019-01-24 11:17:15 +0100

    Convert to typescript

--- src/cryptor.test.ts
+++ src/cryptor.test.ts
@@ -1,21 +1,16 @@
+import { expect } from "chai"
+import * as cryptor from "./index"
+
 describe('cryptor', () => {
-  let cryptor
-
-  beforeEach(() => {
-    cryptor = window.cryptor
-  })
-
   // Static Cryptor methods
-  it('can generate an AES GCM key', done => {
-    cryptor.generateSymmetricKey().then(key => {
-      expect(key.algorithm.name).toEqual('AES-GCM')
-      expect(key.algorithm.length).toEqual(256)
-      expect(key.extractable).toBeTruthy()
-      done()
-    })
+  it('can generate an AES GCM key', async () => {
+    const key = await cryptor.generateSymmetricKey()
+    expect(key.algorithm.name).to.equal('AES-GCM')
+    expect((key.algorithm as AesKeyAlgorithm).length).to.equal(256)
+    expect(key.extractable).to.be.ok
   })
 
-  it('can encrypt/decrypt utf8 strings using AES-GCM', async done => {
+  it('can encrypt/decrypt utf8 strings using AES-GCM', async () => {
     const pt = 'Hello world'
     const key = await cryptor.generateSymmetricKey()
     const data = await cryptor.encryptSymmetric(pt, key)
@@ -25,22 +20,19 @@ describe('cryptor', () => {
       data.iv,
       data.additionalData
     )
-    expect(new TextDecoder('utf-8').decode(dt)).toEqual(pt)
-    done()
+    expect(new TextDecoder('utf-8').decode(dt)).to.equal(pt)
   })
 
-  it('can generate an RSA-OAEP key', done => {
-    cryptor.generateKeypair().then(keyPair => {
-      const { publicKey, privateKey } = keyPair
-      expect(publicKey.algorithm.name).toEqual('RSA-OAEP')
-      expect(publicKey.algorithm.modulusLength).toEqual(2048)
-      expect(privateKey.algorithm.name).toEqual('RSA-OAEP')
-      expect(privateKey.algorithm.modulusLength).toEqual(2048)
-      done()
-    })
+  it('can generate an RSA-OAEP key', async () => {
+    const keyPair = await cryptor.generateKeypair()
+    const { publicKey, privateKey } = keyPair
+    expect(publicKey.algorithm.name).to.equal('RSA-OAEP')
+    expect((publicKey.algorithm as RsaKeyAlgorithm).modulusLength).to.equal(2048)
+    expect(privateKey.algorithm.name).to.equal('RSA-OAEP')
+    expect((privateKey.algorithm as RsaKeyAlgorithm).modulusLength).to.equal(2048)
   })
 
-  it('can wrap/unwrap an AES-GCM key using RSA-OAEP', async done => {
+  it('can wrap/unwrap an AES-GCM key using RSA-OAEP', async () => {
     const pt = 'Hello world'
     const key = await cryptor.generateSymmetricKey()
     const data = await cryptor.encryptSymmetric(pt, key)
@@ -53,50 +45,45 @@ describe('cryptor', () => {
       data.iv,
       data.additionalData
     )
-    expect(new TextDecoder('utf-8').decode(dt)).toEqual(pt)
-    done()
+    expect(new TextDecoder('utf-8').decode(dt)).to.equal(pt)
   })
 
-  it('can derive an AES 256-bit key from a passphrase using PBKDF2', async done => {
+  it('can derive an AES 256-bit key from a passphrase using PBKDF2', async () => {
     const key = await cryptor.deriveKeyFromPassphrase('Password', 'salt')
     const exported = await window.crypto.subtle.exportKey('jwk', key)
-    expect(exported.alg).toEqual('A256GCM')
-    expect(exported.k).toEqual('_60NZIiD0VOqB-rwzptTLW3S2UBRfXlpItgSxqjLmew')
-    done()
+    expect(exported.alg).to.equal('A256GCM')
+    expect(exported.k).to.equal('_60NZIiD0VOqB-rwzptTLW3S2UBRfXlpItgSxqjLmew')
   })
 
-  it('can derive an arbitrary number of bits from a passphrase using PBKDF2', async done => {
+  it('can derive an arbitrary number of bits from a passphrase using PBKDF2', async () => {
     const bits = await cryptor.deriveBitsFromPassphrase('password', 'salt', 64)
-    expect(Array.from(bits)).toEqual([3, 148, 162, 237, 227, 50, 201, 161])
-    done()
+    expect(Array.from(bits)).to.equal([3, 148, 162, 237, 227, 50, 201, 161])
   })
 
-  it('can generate authBits to be used for authentication', async done => {
+  it('can generate authBits to be used for authentication', async () => {
     const authBits = await cryptor.generateAuthBits('password', 'salt')
     let c = new cryptor.Cryptor()
     await c.generate('password', 'salt')
-    expect(c.authBits).toEqual(authBits)
-    done()
+    expect(c.authBits).to.equal(authBits)
   })
 
   // Instance methods
-  it('can generate new instance of Cryptor from a passphrase', async done => {
+  it('can generate new instance of Cryptor from a passphrase', async () => {
     let c = new cryptor.Cryptor()
     await c.generate('password', 'salt')
-    expect(c.keyPair).toBeDefined()
-    expect(c.masterKey).toBeDefined()
-    expect(c.authBits).toBeDefined()
-    done()
+    expect(c.keyPair).to.not.be.undefined
+    expect(c.masterKey).to.not.be.undefined
+    expect(c.authBits).to.not.be.undefined
   })
 
-  it('can serialize/deserialize an instance of Cryptor', async done => {
+  it('can serialize/deserialize an instance of Cryptor', async () => {
     const c = new cryptor.Cryptor()
     await c.generate('password', 'salt')
     const json = await c.toJSON()
     let c2 = new cryptor.Cryptor()
     await c2.fromJSON(json, 'password', 'salt')
 
-    expect(c.authBits).toEqual(c2.authBits)
+    expect(c.authBits).to.equal(c2.authBits)
 
     // We generate a random key and RSA encrypt it with c.
     // We then use that key to encrypt a test plain text.
@@ -117,7 +104,6 @@ describe('cryptor', () => {
       data.additionalData
     )
 
-    expect(new TextDecoder('utf-8').decode(dt)).toEqual(pt)
-    done()
+    expect(new TextDecoder('utf-8').decode(dt)).to.equal(pt)
   })
 })
---
+++ src/index.ts
@@ -0,0 +1,285 @@
+// Helper to extract argument types from a function
+type ArgumentsType<T> = T extends (...args: infer A) => any ? A : never;
+
+  interface IJSONCryptoKey {
+    ct: Uint8Array
+    iv: Uint8Array
+    additionalData: Uint8Array
+  }
+
+  interface IBase64JSONCryptoKey {
+    ct: string
+    iv: string
+    additionalData: string
+  }
+  
+  /**
+   * JSON representation of a key pair.
+   * 
+   * This structured is designed to be transmitted. The private key is encrypted.
+   */
+  interface ICryptorKeyPair {
+    privateKeyEncrypted: IBase64JSONCryptoKey,
+    publicKey: JsonWebKey,
+    authBits: Uint8Array,
+  }
+
+  const MASTER_KEY_USAGES: KeyUsage[] = ['encrypt', 'decrypt']
+  const MASTER_KEY_ALGORITHM: AesKeyAlgorithm = { name: 'AES-GCM', length: 256 }
+
+  const SYMMETRIC_KEY_USAGES: KeyUsage[] = ["decrypt", "encrypt", "unwrapKey"]
+  const SYMMETRIC_KEY_ALGORITHM: RsaHashedKeyAlgorithm = {
+      name: 'RSA-OAEP',
+      modulusLength: 2048,
+      publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+      hash: { name: 'SHA-256' },
+  }
+
+  const ASYMMETRIC_KEY_USAGES: KeyUsage[] = ['wrapKey', 'wrapKey']
+  const ASYMMETRIC_KEY_ALGORITHM: RsaHashedKeyAlgorithm = {
+    name: 'RSA-OAEP',
+    modulusLength: 2048,
+    publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+    hash: { name: 'SHA-256' },
+  }
+
+  const SYMMETRIC_ENCRYPTION_ALGORITHM = {
+    name: 'AES-GCM',
+    tagLength: 128,
+  }
+
+  const KEY_WRAP_ALGORITHM: RsaHashedKeyAlgorithm = {
+      name: 'RSA-OAEP',
+      modulusLength: 2048,
+      publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+      hash: { name: 'SHA-256' },
+  }
+
+  /**
+   * Convert input to UTF-8 encoded Uint8Array
+   * 
+   * @param input Data to convert. If this is a string it is UTF-8 encoded.
+   */
+  const toUint8Array = (input: string | Uint8Array): Uint8Array => typeof input === 'string' ? new TextEncoder().encode(input) : input
+
+
+  export function generateSymmetricKey() {
+      return crypto.subtle.generateKey(
+        {
+          name: 'AES-GCM',
+          length: 256,
+        },
+        true, // make key extractable
+        ['encrypt', 'decrypt']
+      )
+    }
+
+    export function encryptSymmetric(pt: string | Uint8Array, key: CryptoKey): PromiseLike<IJSONCryptoKey> {
+      const iv = crypto.getRandomValues(new Uint8Array(12))
+      const additionalData = crypto.getRandomValues(new Uint8Array(12))
+      return crypto.subtle.encrypt(
+          {
+            ...SYMMETRIC_ENCRYPTION_ALGORITHM,
+            iv,
+            additionalData,
+          } as AesGcmParams,
+          key,
+          toUint8Array(pt)
+        )
+        .then(ct => {
+          return {
+            ct: new Uint8Array(ct),
+            iv,
+            additionalData,
+          }
+        })
+    }
+
+    export function decryptSymmetric(ct: ArgumentsType<SubtleCrypto['decrypt']>[2], key: CryptoKey, iv: AesGcmParams['iv'], additionalData: AesGcmParams['additionalData']) {
+      return crypto.subtle.decrypt(
+        {
+          ...SYMMETRIC_ENCRYPTION_ALGORITHM,
+          iv,
+          additionalData,
+        },
+        key,
+        ct
+      )
+    }
+
+    export function generateKeypair() {
+      return crypto.subtle.generateKey(ASYMMETRIC_KEY_ALGORITHM,
+        true, // make key extractable
+        ASYMMETRIC_KEY_USAGES,
+      )
+    }
+
+    export function wrapKey(key: CryptoKey, publicKey: CryptoKey) {
+      return crypto.subtle.wrapKey('raw', key, publicKey, KEY_WRAP_ALGORITHM)
+    }
+
+    export function unwrapKey(wrapped: ArgumentsType<SubtleCrypto['unwrapKey']>[1], privateKey: CryptoKey) {
+      return crypto.subtle.unwrapKey(
+        'raw',
+        wrapped,
+        privateKey,
+        KEY_WRAP_ALGORITHM,
+        SYMMETRIC_KEY_ALGORITHM,
+        true,
+        SYMMETRIC_KEY_USAGES
+      )
+    }
+
+    export function deriveBitsFromPassphrase(passphrase: string | Uint8Array, salt: string | Uint8Array, bits: number) {
+      return crypto.subtle.importKey(
+          'raw',
+          toUint8Array(passphrase),
+          {
+            name: 'PBKDF2',
+          } as Pbkdf2Params,
+          false,
+          ['deriveBits']
+        )
+        .then(baseKey =>
+          crypto.subtle.deriveBits(
+            {
+              name: 'PBKDF2',
+              salt: toUint8Array(salt),
+              iterations: 100000,
+              hash: { name: 'SHA-256' },
+            } as Pbkdf2Params,
+            baseKey,
+            bits
+          )
+        )
+        .then(bits => new Uint8Array(bits))
+    }
+
+    export function deriveKeyFromPassphrase(passphrase: string | Uint8Array, salt: string | Uint8Array) {
+      return crypto.subtle.importKey(
+          'raw',
+          toUint8Array(passphrase),
+          {
+            name: 'PBKDF2',
+          } as Pbkdf2Params,
+          false,
+          ['deriveKey']
+        )
+        .then(baseKey =>
+          crypto.subtle.deriveKey(
+            {
+              name: 'PBKDF2',
+              salt: toUint8Array(salt),
+              iterations: 100000,
+              hash: { name: 'SHA-256' },
+            } as Pbkdf2Params,
+            baseKey,
+            { name: 'AES-GCM', length: 256 },
+            true,
+            ['encrypt', 'decrypt']
+          )
+        )
+    }
+
+    export function uInt8ArrayToB64(array: Uint8Array) {
+      return btoa(
+        Array.from(array)
+          .map(byte => String.fromCharCode(byte))
+          .join('')
+      )
+    }
+
+    export function b64ToUint8Array(b64: string) {
+      return new Uint8Array(
+        atob(b64)
+          .split('')
+          .map(c => c.charCodeAt(0))
+      )
+    }
+
+    export async function generateAuthBits(passphrase: string | Uint8Array, salt: string | Uint8Array) {
+      const bits = await deriveBitsFromPassphrase(passphrase, salt, 512)
+      return bits.slice(32, 32)
+    }
+
+export class Cryptor {
+  public authBits: Uint8Array
+  public keyPair: CryptoKeyPair
+  public masterKey: CryptoKey
+
+    async generate(passphrase: string | Uint8Array, salt: string | Uint8Array) {
+      this.keyPair = await generateKeypair()
+      const bits = await deriveBitsFromPassphrase(passphrase, salt, 512)
+      this.masterKey = await crypto.subtle.importKey(
+        'raw',
+        bits.slice(0, 32).buffer,
+        MASTER_KEY_ALGORITHM,
+        true,
+        MASTER_KEY_USAGES
+      )
+      this.authBits = bits.slice(32, 32)
+    }
+
+    async toJSON(): Promise<ICryptorKeyPair> {
+      const key = await crypto.subtle.exportKey(
+        'pkcs8',
+        this.keyPair.privateKey
+      ) as Uint8Array
+
+      const privateKeyEncrypted = await encryptSymmetric(
+        key,
+        this.masterKey
+      )
+
+      const b64Key: IBase64JSONCryptoKey = {
+        ct: uInt8ArrayToB64(privateKeyEncrypted.ct),
+        iv: uInt8ArrayToB64(privateKeyEncrypted.iv),
+        additionalData: uInt8ArrayToB64(
+          privateKeyEncrypted.additionalData
+        ),
+      }
+
+      return {
+        privateKeyEncrypted: b64Key,
+        publicKey: await crypto.subtle.exportKey('jwk', this.keyPair.publicKey),
+        authBits: this.authBits,
+      }
+    }
+
+    async fromJSON(json: ICryptorKeyPair, passphrase: string | Uint8Array, salt: string | Uint8Array) {
+      const bits = await deriveBitsFromPassphrase(passphrase, salt, 512)
+      this.masterKey = await crypto.subtle.importKey(
+        'raw',
+        bits.slice(0, 32).buffer,
+        MASTER_KEY_ALGORITHM,
+        true,
+        MASTER_KEY_USAGES
+      )
+      this.authBits = bits.slice(32, 32)
+      let { privateKeyEncrypted } = json
+
+      const ct = b64ToUint8Array(privateKeyEncrypted.ct)
+      const iv = b64ToUint8Array(privateKeyEncrypted.iv)
+      const additionalData = b64ToUint8Array(privateKeyEncrypted.additionalData)
+
+      const decryptedKey = await decryptSymmetric(
+        ct,
+        this.masterKey,
+        iv,
+        additionalData
+      )
+
+      const publicKey = await  crypto.subtle.importKey("jwk", json.publicKey, ASYMMETRIC_KEY_ALGORITHM, true, ASYMMETRIC_KEY_USAGES)
+      const privateKey = await crypto.subtle.importKey(
+        'pkcs8',
+        decryptedKey,
+        SYMMETRIC_KEY_ALGORITHM as RsaHashedImportParams,
+        true,
+        SYMMETRIC_KEY_USAGES
+      )
+      this.keyPair = { publicKey, privateKey }
+    }
+  }
+
+  export default Cryptor
+  
